opendocx/Open-Xml-PowerTools

commit e1cc7e76ec84f5e6bcbd57b68874a1da8104dc1a

emulate Word's treatment of xml:space="preserve"
(when converting DOCX content to Unicode strings)

OpenXmlPowerTools/UnicodeMapper.cs
@@ -69,7 +69,12 @@ namespace OpenXmlPowerTools
 
             // For w:t elements, we obviously want the element's value.
             if (element.Name == W.t)
-                return (string) element;
+            {
+                // Emulate Word's handling of the xml:space attribute on text elements
+                XAttribute spaceAttribute = element.Attribute(XNamespace.Xml + "space");
+                string space = spaceAttribute != null ? spaceAttribute.Value : null;
+                return space == "preserve" ? (string)element : IgnoreTextSpacing((string)element);
+            }
 
             // Turn elements representing special characters into their corresponding
             // unicode characters.
@@ -123,6 +128,22 @@ namespace OpenXmlPowerTools
             // Elements we don't recognize will be turned into a character that
             // doesn't typically appear in documents.
             return StartOfHeading.ToString();
+        }
+
+        /// <summary>
+        /// Emulate the way Word treats text elements when attribute xml:space="preserve"
+        /// is NOT present.
+        /// </summary>
+        /// <param name="text">The entire content of the w:t element.</param>
+        /// <returns>The corresponding text string Word would display, print, and
+        /// allow to be edited.</returns>
+        private static string IgnoreTextSpacing(string text)
+        {
+            // all whitespace at beginning and end of entire string is ignored
+            // if text contains line breaks, they are ignored/replaced with a single space
+            return string.Join(" ",
+                text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+            ).Trim();
         }
 
         /// <summary>

OpenXmlPowerTools.Tests/UnicodeMapperTests.cs
@@ -142,6 +142,40 @@ namespace OpenXmlPowerTools.Tests
             Assert.Equal(symFromChar1.ToString(SaveOptions.None), symFromChar2.ToString(SaveOptions.None));
             Assert.Equal(symFromChar1.ToString(SaveOptions.None), symFromChar3.ToString(SaveOptions.None));
         }
+
+        [Fact]
+        public void HonorsXmlSpace()
+        {
+            XDocument partDocument = XDocument.Parse(PreserveSpacingXmlString);
+            XElement p = partDocument.Descendants(W.p).Last();
+            string innerText = p.Descendants(W.r)
+                .Select(UnicodeMapper.RunToString)
+                .StringConcatenate();
+            Assert.Equal(@"The following space is retained: but this one is not:. Similarly these two lines should have only a space between them: Line 1! Line 2!", innerText);
+        }
+
+        private const string PreserveSpacingXmlString =
+@"<w:document xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main"">
+  <w:body>
+    <w:p>
+      <w:r>
+        <w:t xml:space=""preserve"">The following space is retained: </w:t>
+      </w:r>
+      <w:r>
+        <w:t>but this one is not: </w:t>
+      </w:r>
+      <w:r>
+        <w:t xml:space=""preserve"">. Similarly these two lines should have only a space between them: </w:t>
+      </w:r>
+      <w:r>
+        <w:t>
+          Line 1!
+Line 2!
+        </w:t>
+      </w:r>
+    </w:p>
+  </w:body>
+</w:document>";
     }
 }
 

commit da10f39698f1e68b23ec096b88ff45310ca7b226

fixed OpenXmlRegex when runs contain non-breaking spaces (regression discovered from commit e1cc7e76 on 8 Jul 2023); add more unit tests.

OpenXmlPowerTools/UnicodeMapper.cs
@@ -139,11 +139,13 @@ namespace OpenXmlPowerTools
         /// allow to be edited.</returns>
         private static string IgnoreTextSpacing(string text)
         {
-            // all whitespace at beginning and end of entire string is ignored
-            // if text contains line breaks, they are ignored/replaced with a single space
-            return string.Join(" ",
-                text.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
-            ).Trim();
+            // If text contains line breaks (as it MAY if there are line breaks
+            // in the w:t content), Word replaces those with a single space.
+            return string
+                .Join(" ", text.Split(LineFeed)) // XML parsing already normalized to LF
+                .Trim(' ', HorizontalTabulation); // ignore leading & trailing spaces/tabs
+            // Trim() must be given explicit characters to remove; otherwise it eliminates
+            // SIGNIFICANT whitespace as well (such as non-breaking spaces).
         }
 
         /// <summary>

OpenXmlPowerTools.Tests/OpenXmlRegexTests.cs
@@ -217,6 +217,60 @@ namespace OpenXmlPowerTools.Tests
   </w:body>
 </w:document>";
 
+        private const string NonBreakingSpacesAndLineBreaksXmlString =
+@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes""?>
+<w:document xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main"">
+  <w:body>
+    <w:p>
+      <w:r>
+        <w:t>First line</w:t>
+      </w:r>
+      <w:r>
+        <w:t>               </w:t>
+      </w:r>
+      <w:proofErr w:type=""gramStart""/>
+      <w:r>
+        <w:t xml:space=""preserve"">  </w:t>
+      </w:r>
+      <w:r>
+        <w:t>!</w:t>
+      </w:r>
+      <w:proofErr w:type=""gramEnd""/>
+      <w:r>
+        <w:br/>
+        <w:t>                                         </w:t>
+      </w:r>
+      <w:r>
+        <w:t>!</w:t>
+      </w:r>
+      <w:r>
+        <w:br/>
+      </w:r>
+      <w:r>
+        <w:t>Last line</w:t>
+      </w:r>
+      <w:r>
+        <w:t xml:space=""preserve""> </w:t>
+      </w:r>
+      <w:r>
+        <w:t>{</w:t>
+      </w:r>
+      <w:r>
+        <w:t>find and replace me!</w:t>
+      </w:r>
+      <w:r>
+        <w:t>}</w:t>
+      </w:r>
+      <w:r>
+        <w:t xml:space=""preserve"">  </w:t>
+      </w:r>
+      <w:r>
+        <w:t>!</w:t>
+      </w:r>
+    </w:p>
+  </w:body>
+</w:document>";
+
         private static string InnerText(XContainer e)
         {
             return e.Descendants(W.r)
@@ -420,6 +474,33 @@ namespace OpenXmlPowerTools.Tests
 
                 Assert.Equal(2, count);
                 Assert.Equal("ThisIsAParagraphContainingNoNaturalLBsSoTheLBIsForced.", innerText);
+            }
+        }
+
+        [Fact]
+        public void CanMatchDespiteNoBreakSpacesAndLineBreaks()
+        {
+            XDocument partDocument = XDocument.Parse(NonBreakingSpacesAndLineBreaksXmlString);
+            XElement p = partDocument.Descendants(W.p).Last();
+            string innerText = InnerText(p);
+
+            Assert.Equal("First line                 !\r                                         !\rLast line {find and replace me!}  !", innerText);
+
+            using (var stream = new MemoryStream())
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(stream, DocumentType))
+            {
+                MainDocumentPart part = wordDocument.AddMainDocumentPart();
+                part.PutXDocument(partDocument);
+
+                IEnumerable<XElement> content = partDocument.Descendants(W.p);
+                var regex = new Regex(@"{find and replace me!}");
+                int count = OpenXmlRegex.Replace(content, regex, "REPLACED", null);
+
+                p = partDocument.Descendants(W.p).Last();
+                innerText = InnerText(p);
+
+                Assert.Equal(1, count);
+                Assert.Equal("First line                 !\r                                         !\rLast line REPLACED  !", innerText);
             }
         }
 

OpenXmlPowerTools.Tests/UnicodeMapperTests.cs
@@ -176,6 +176,54 @@ Line 2!
     </w:p>
   </w:body>
 </w:document>";
+
+        [Theory]
+        [InlineData("Line 1\rLine 2\r",           "Line 1 Line 2",    "Line 1\nLine 2\n"      )]
+        [InlineData("   Line 1\r   Line 2\r",     "Line 1    Line 2", "   Line 1\n   Line 2\n")]
+        [InlineData("Line 1\nLine 2\n",           "Line 1 Line 2",    "Line 1\nLine 2\n"      )]
+        [InlineData("   Line 1\n   Line 2\n",     "Line 1    Line 2", "   Line 1\n   Line 2\n")]
+        [InlineData("Line 1\r\nLine 2\r\n",       "Line 1 Line 2",    "Line 1\nLine 2\n"      )]
+        [InlineData("   Line 1\r\n   Line 2\r\n", "Line 1    Line 2", "   Line 1\n   Line 2\n")]
+        public void HandlesDifferentLineEndingsWhenPreservingXmlSpace(string multiLineString, string expect1, string expect2)
+        {
+            var xmlString = PreserveSpacingMultiLineXmlString.Replace("XXX", multiLineString);
+            XDocument partDocument = XDocument.Parse(xmlString);
+            XElement r1 = partDocument.Descendants(W.r).First();
+            string innerText1 = UnicodeMapper.RunToString(r1);
+            XElement r2 = partDocument.Descendants(W.r).Last();
+            string innerText2 = UnicodeMapper.RunToString(r2);
+            Assert.Equal(expect1, innerText1);
+            Assert.Equal(expect2, innerText2);
+        }
+
+        private const string PreserveSpacingMultiLineXmlString = @"<w:document xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main""><w:body>"
+            + @"<w:p><w:r><w:t>XXX</w:t></w:r></w:p>"
+            + @"<w:p><w:r><w:t xml:space=""preserve"">XXX</w:t></w:r></w:p>"
+            + @"</w:body></w:document>";
+
+        [Fact]
+        public void HonorsNonBreakingWhitespace()
+        {
+            XDocument partDocument = XDocument.Parse(SignificantWhitespaceXmlString);
+            XElement p = partDocument.Descendants(W.p).Last();
+            string innerText = p.Descendants(W.r)
+                .Select(UnicodeMapper.RunToString)
+                .StringConcatenate();
+            Assert.Equal(@"Regular spaces:Mixed non-breaking spaces:     ", innerText);
+            // all regular spaces are trimmed. non-breaking spaces are not.
+            // trailing spaces after the colon are: sp nbsp nbsp sp nbsp
+            // (trailing space was trimmed)
+        }
+
+        private const string SignificantWhitespaceXmlString =
+@"<w:document xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main"">
+  <w:body>
+    <w:p>
+      <w:r><w:t> Regular spaces:      </w:t></w:r>
+      <w:r><w:t> Mixed non-breaking spaces:      </w:t></w:r>
+    </w:p>
+  </w:body>
+</w:document>";
     }
 }
 